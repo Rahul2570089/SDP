784a70f2ab8a6b1ce074ecc756371eb357bbf428
rahul <rs2570089@gmail.com>
2023-05-04 21:30:26 +0530

    Government schemes updated

--- pullventure_flutter/lib/homepages/government_schemes/government_schemes.dart
+++ pullventure_flutter/lib/homepages/government_schemes/government_schemes.dart
@@ -10,55 +10,39 @@ class GovernmentSchemes extends StatefulWidget {
 
 class _GovernmentSchemesState extends State<GovernmentSchemes> {
   List<String> titles = [
-    "Support for International Patent Protection in Electronics and & Information Technology (SIP-EIT)",
-    "Stand-Up India for Financing SC/ST and/or Women Entrepreneurs",
-    "Single Point Registration Scheme",
-    "ExtraMural Research or Core Research Grant",
-    "High Risk -High Reward Research",
-    "IREDA NCEF Refinance Scheme",
-    "Dairy Entrepreneurship Development Scheme",
-    "Revamped Scheme of Fund for Regeneration of Traditional Industries (SFURTI)",
-    "Assistance to Professional Bodies & Seminars/Symposia",
-    "Software Technology Park Scheme"
+    "Scheme to Support IPR Awareness Workshop/Seminars in E&IT sector",
+    "Science and Technology (S&T) for Coir Institutions",
+    "Technology Development Programme",
+    "Drugs and Pharmaceutical Research",
+    "Small Business Innovation Research Initiative (SBIRI)",
+    "Financial Assistance to Professional Bodies & Seminars/ Symposia"
   ];
 
   List<String> subtitles = [
     "Ministry Of Electronics & Information Technology",
-    "Small Industries Development Bank of India (SIDBI)",
-    "Ministry of Micro Small & Medium Enterprises",
-    "Science and Engineering Research Board (SERB) under Ministry of Science & Technology",
-    "Science and Engineering Research Board (SERB) under Department of Science & Technology",
-    "Indian Renewable Energy Development Agency (IREDA)",
-    "National Bank for Agriculture and Rural Development (NABARD)",
-    "Khadi and Village Industries Commission under Ministry Of MSME",
+    "Ministry of Micro, Small and Medium Enterprises",
+    "Ministry of Science & Technology, Department of Science and Technology",
     "Department Of Science & Technology",
-    "Software Technology Parks of India Under Ministry of Electronics and Information Technology"
+    "Ministry of Science & Technology, Biotechnology Industry Research Assistance Council (BIRAC)",
+    "Ministry of Science & Technology, NSERB INDIA"
   ];
 
   List<String> images = [
     "assets/images/1.png",
-    "assets/images/2.jpg",
     "assets/images/3.png",
     "assets/images/4.png",
-    "assets/images/5.png",
-    "assets/images/6.png",
+    "assets/images/4.png",
     "assets/images/7.jpg",
-    "assets/images/8.png",
-    "assets/images/9.png",
-    "assets/images/10.png"
+    "assets/images/4.png",
   ];
 
   List<String> urls = [
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/international-patent-protection-sip-eit.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/stand-up-india.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/single-point-registration.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/extra-mural-research.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/high-risk-high-reward.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/national-clean-energy-fund-Refinance.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/dairy-entrepreneurship-development-scheme.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/revamped-scheme.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/assistance-to-professional-bodies.html",
-    "https://www.startupindia.gov.in/content/sih/en/government-schemes/software-technology-park-scheme.html"
+    "https://www.indiascienceandtechnology.gov.in/funding-opportunities/startups/scheme-support-ipr-awareness-seminarsworkshops-eit-sector",
+    "https://msme.gov.in/sites/default/files/Revised_Operation_Guidelines_of_CVY.pdf",
+    "https://dst.gov.in/technology-development-program-tdp",
+    "https://dst.gov.in/drugs-pharmaceutical-research",
+    "https://birac.nic.in/desc_new.php?id=217",
+    "https://serbonline.in/SERB/seminar_symposia"
   ];
 
   Widget listview() {
